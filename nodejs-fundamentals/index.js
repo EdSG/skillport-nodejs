561ffb260684cf1a0952f94d357f55a3a763a310
Edgar Sánchez Grajeda <edgarsanchezg@gmail.com>
2017-09-09 12:16:47 -0700

    Sample using modules: exports and require.

---
+++ nodejs-fundamentals/index.js
@@ -0,0 +1,12 @@
+console.log("A simple node application");
+
+// Requires and Exports
+
+var myModule = require('./sample_module');
+
+console.log(myModule);
+
+//console.log(myModule.randomNum());
+
+console.log(myModule());
+
---
+++ nodejs-fundamentals/sample_module.js
@@ -0,0 +1,11 @@
+/*
+exports.firstName = "Andy";
+
+exports.randomNum = function() {
+  return Math.random();
+}
+*/
+
+module.exports = function() {
+  return Math.random();
+}
